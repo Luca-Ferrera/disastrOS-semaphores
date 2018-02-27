7a0f31efc3b35794052ffcf66f76414dc7d75f24
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-02-27 11:57:24 +0100

    Prettified debug messages

--- src/disastrOS_semopen.c
+++ src/disastrOS_semopen.c
@@ -12,7 +12,7 @@ void internal_semOpen(){
   Semaphore* sem = (Semaphore*) running->syscall_args[0];
   int sem_id = running->syscall_args[1];
   int count = running->syscall_args[2];
-  printf("PID %d requested open of semaphore %d\n", running->pid, sem_id);
+  printf("[*] PID %d requested open of semaphore %d\n", running->pid, sem_id);
 
   // Initialize the semaphore once
   if (!(initialized++)) Semaphore_init();
@@ -20,12 +20,12 @@ void internal_semOpen(){
   // Check if a semaphore with the same ID is already open
   sem = SemaphoreList_byId(&running->sem_descriptors, sem_id);
   if (sem) {
-    printf("Semaphore with id %d already existing, returning it\n", sem_id);
+    printf("[*] Semaphore with id %d already existing, returning it\n", sem_id);
     running->syscall_retvalue = sem->id;
   }
 
   // Create a semaphore with the given ID
-  printf("Semaphore with id %d doesn't exist, allocating it\n", sem_id);
+  printf("[+] Semaphore with id %d doesn't exist, allocating it\n", sem_id);
   sem = Semaphore_alloc(sem_id, count);
   running->syscall_retvalue = sem->id;
 }

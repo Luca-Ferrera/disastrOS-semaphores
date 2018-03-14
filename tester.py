9008a3368b31055933b9d747fae91202fe3542bb
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-03-14 18:32:30 +0100

    Now redirecting output to null

--- tester.py
+++ tester.py
@@ -5,10 +5,11 @@ import subprocess
 ATTEMPTS = int(sys.argv[1]) if len(sys.argv) > 1 else 10 
 errors = 0
 
-for _ in range(ATTEMPTS):
-    s = subprocess.Popen(['./src/disastrOS_semaphore_test'])
-    out, err = s.communicate()
-    errors += 1 if s.returncode != 0 else 0
+with open('/dev/null', 'wb') as null:
+    for _ in range(ATTEMPTS):
+        s = subprocess.Popen(['./src/disastrOS_semaphore_test'], stdout=null, stderr=null)
+        out, err = s.communicate()
+        errors += 1 if s.returncode != 0 else 0
 
 print('=====================' + '=' * ( len(str(errors)) + len(str(ATTEMPTS)) ))
 print('Failure rate: {}/{} times'.format(errors, ATTEMPTS))

e104a2d3173d2c936e358ce1f1702d7dd57b58c1
Luca Ferrera <luca.ferrera96@gmail.com>
2018-03-14 17:47:19 +0100

    check if process owns the semaphore

--- src/disastrOS_semwait.c
+++ src/disastrOS_semwait.c
@@ -10,7 +10,7 @@ void internal_semWait(){
   int sem_fd = running->syscall_args[0];
   
   printf("Pid %d requesting semwait on %d semaphore\n", running->pid, sem_fd);
-
+  
   SemDescriptor* sem_desc = SemDescriptorList_byFd(&(running->sem_descriptors), sem_fd);
   printf("Sem_desc %d \n", sem_desc);
   if(!sem_desc){
@@ -20,18 +20,22 @@ void internal_semWait(){
   }
   printf("no errore\n");
   //TODO: check if process owns the semaphore
+  if(!List_find(&running->sem_descriptors, sem_desc)){
+    printf("[*] process %d, doesn't own sem_desc %d\n", running->pid, sem_desc);
+    running->syscall_retvalue = DSOS_ESEMAPHOREWAIT;
+    return;
+  }
   Semaphore* sem = sem_desc->semaphore;
+  
   sem->count-=1;
-  if(sem->count < 0){
+  //changed condition for testing purpose
+  if(sem->count >= 0){
     //put running process into waiting_list
     List_insert(&waiting_list, waiting_list.last, (ListItem*)running);
-    List_insert(&(sem->waiting_descriptors),sem->waiting_descriptors.last,(ListItem*)sem_desc);
+    List_insert(&(sem->waiting_descriptors),(sem->waiting_descriptors).last,(ListItem*)sem_desc);
     //scheduling next process
     running->syscall_retvalue=0;
-    printf("\nSemaphores: \n");
-    SemaphoreList_print(&semaphores_list);
-    printf("\nWaiting: \n");
-    PCBList_print(&waiting_list);
+    disastrOS_printStatus();
     disastrOS_preempt();
   }
   running->syscall_retvalue=0;
